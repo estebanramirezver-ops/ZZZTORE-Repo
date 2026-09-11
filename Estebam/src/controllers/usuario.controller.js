ec81c1610b2a61569526676fc20e9a30a4c9e276
MiguelAngel <argelts1403@gmail.com>
2026-09-11 16:39:24 -0500

    Borrado de carpeta equivocada

--- Estebam/src/controllers/usuario.controller.js
+++
@@ -1,6 +0,0 @@
-import { listarUsuarios } from "../services/usuario.service.js";
-
-export const getUsuarios = async (req, res) => {
-    const usuarios = await listarUsuarios();
-    return res.json(usuarios);
-};
--- Estebam/src/routes/usuario.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { getUsuarios } from "../controllers/usuario.controller.js";
-
-const router = Router();
-router.get("/", getUsuarios);
-export default router;
